7e798af48f64a79d1cfe2105ba0a8b528024c9b6
Duber1234 <pescafelipe@misena.edu.co>
2016-01-30 22:55:09 -0500

    la cage

--- juzgado/juzgado/Vistas/Login/frmProcesosJudiciales.cs
+++ juzgado/juzgado/Vistas/Login/frmProcesosJudiciales.cs
@@ -24,7 +24,7 @@ namespace juzgado.Vistas.Login
             InitializeComponent();
             selectMenu(4);
             llenarTabla();
-          
+            
          
             dgvInvolucrados.ContextMenuStrip = null;
         }
